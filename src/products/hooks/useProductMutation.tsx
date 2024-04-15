342524b3269b047e12492cebffe4eaeea5ff6505
CKIN7 <covain77@gmail.com>
2024-04-14 22:11:51 -0400

    removeQueries

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -5,32 +5,45 @@ import { Product, productActions } from ".."
 
 export const useProductMutation = () => {
 
-    const queryClient = useQueryClient()
-  
-    const mutation = useMutation({
-        mutationFn: productActions.createProduct,
-        onMutate: (product) => {
-          const optimistProduct = {id: Math.random(), ...product}
+  const queryClient = useQueryClient()
 
-          queryClient.setQueryData<Product[]>(
-            ['products', { filterKey: product.category }],
-            (old) => {
-              if (!old) return [optimistProduct]
-               return [...old, optimistProduct]
-            })
-          
-        },  
-        onSuccess:  (product) => {
-          queryClient.setQueryData<Product[]>(
-            ['products', { filterKey: product.category }],
-            (old) => {
-              if (!old) return [product];
-               return [...old, product]
+  const mutation = useMutation({
+    mutationFn: productActions.createProduct,
+    onMutate: (product) => {
+      const optimisticProduct = { id: Math.random(), ...product }
+
+      queryClient.setQueryData<Product[]>(
+        ['products', { filterKey: product.category }],
+        (old) => {
+          if (!old) return [optimisticProduct]
+          return [...old, optimisticProduct]
+        })
+
+      return {
+        optimisticProduct: optimisticProduct
+      }
+
+    },
+    onSuccess: (product, _, context) => {
+
+      queryClient.removeQueries({
+        queryKey: ['product', context?.optimisticProduct.id]
+      })
+      
+      queryClient.setQueryData<Product[]>(
+        ['products', { filterKey: product.category }],
+        (old) => {
+          if (!old) return [product];
+          // return [..old, product]
+
+          return old.map(cacheProduct => {
+            return cacheProduct.id === context?.optimisticProduct.id ? product : cacheProduct
           })
+        })
 
-             
-        }
-    })
 
-    return mutation
+    }
+  })
+
+  return mutation
 }
